bd0fa592d1714bc6a48df034b0429789d1ffd1b2
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-02-17 21:58:56 +0400

    Added Checkbox for filter

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,18 @@ function App(): JSX.Element {
   const [passwordHistory, setPasswordHistory] = useState<string[]>([]);
   const [passwordLength, setPasswordLength] = useState<number>(15); // State to manage password length
   const [passwordStrength, setPasswordStrength] = useState<number>(0); // State to manage password strength
+  const [includeUppercase, setIncludeUppercase] = useState<boolean>(true);
+  const [includeLowercase, setIncludeLowercase] = useState<boolean>(true);
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(true);
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false); // Initially false
 
   const generatePassword = () => {
-    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(?)-_=+{}[]|;:,.<>';
+    let charset = '';
+    if (includeUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (includeLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (includeNumbers) charset += '0123456789';
+    if (includeSymbols) charset += '!@#$%^&*(?)-_=+{}[]|;:,.<>';
+    
     let newPassword = '';
     for (let i = 0; i < passwordLength; i++) {
       const randomIndex = Math.floor(Math.random() * charset.length);
@@ -20,12 +29,31 @@ function App(): JSX.Element {
     // Calculate password strength using zxcvbn
     const strength = zxcvbn(newPassword).score; // Score ranges from 0 to 4
     setPasswordStrength(strength);
-    setPasswordHistory(prevHistory => [newPassword, ...prevHistory.slice(0, 14)]);
   };
 
   const handleLengthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newLength = parseInt(e.target.value);
     setPasswordLength(newLength);
+  };
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    switch (name) {
+      case 'includeUppercase':
+        setIncludeUppercase(checked);
+        break;
+      case 'includeLowercase':
+        setIncludeLowercase(checked);
+        break;
+      case 'includeNumbers':
+        setIncludeNumbers(checked);
+        break;
+      case 'includeSymbols':
+        setIncludeSymbols(checked);
+        break;
+      default:
+        break;
+    }
   };
 
   const passwordStrengthColor = () => {
@@ -48,20 +76,62 @@ function App(): JSX.Element {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-backgroundForBack">
       <header className="text-textColor">
-          <h1 style={{fontSize: '24px'}}>Password Generator</h1>
-        </header>
-        <div style={{width: '540px', height: '80px'}} className="bg-bgMain flex items-center text-center justify-center mt-8">
-  <p className="my-4">
-    <span className="text-secondaryText">{password}</span>
-  </p>
-</div>
-
-
+        <h1 style={{ fontSize: '24px' }}>Password Generator</h1>
+      </header>
+      <div style={{ width: '540px', height: '80px' }} className="bg-bgMain flex items-center text-left justify-between mt-8">
+        <p className="my-4">
+          <span style={{ fontSize: '32px', fontFamily: 'JetBrains Mono' }} className="text-secondaryText text-left">{password}</span>
+        </p>
+      </div>
       <div style={{ width: '540px', height: '528px' }} className="bg-bgMain mt-6">
         <main className="flex-1 flex flex-col justify-center items-center">
           <p className="text-center my-4">
             Welcome to the Random Password Generator. Click the button below to generate a random password.
           </p>
+          <div className="flex justify-between w-full mb-4">
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                name="includeUppercase"
+                checked={includeUppercase}
+                onChange={handleCheckboxChange}
+                className="mr-2"
+              />
+              Include Uppercase Letters
+            </label>
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                name="includeLowercase"
+                checked={includeLowercase}
+                onChange={handleCheckboxChange}
+                className="mr-2"
+              />
+              Include Lowercase Letters
+            </label>
+          </div>
+          <div className="flex justify-between w-full mb-4">
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                name="includeNumbers"
+                checked={includeNumbers}
+                onChange={handleCheckboxChange}
+                className="mr-2"
+              />
+              Include Numbers
+            </label>
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                name="includeSymbols"
+                checked={includeSymbols}
+                onChange={handleCheckboxChange}
+                className="mr-2"
+              />
+              Include Symbols
+            </label>
+          </div>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={generatePassword}>
             Generate
           </button>
